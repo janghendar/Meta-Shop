c3b43b3d2e06a9619bb0ab5c9684cae3b81e1e31
Suhendar Aryadi <suhendararyadi@gmail.com>
2022-01-11 14:53:18 +0700

    Master Meta-Shop

---
+++ lib/constants/app.dart
@@ -0,0 +1,13 @@
+const String baseUrl = 'https://meta-shop.biz/';
+
+const String consumerKey = 'ck_2d7012e8751057494f3298bf5b511be3712ec5c9';
+
+const String consumerSecret = 'cs_5e404705248741500a20dd653e5de896107a8e28';
+
+const String restPrefix = 'wp-json';
+
+const String defaultLanguage = 'en';
+
+const List<String> languageSupport = ['en', 'ar', 'tr'];
+
+const String googleClientId = '27793387154-90mtc1g4bp1nha0jb3n21t8sc2sji9ua.apps.googleusercontent.com';
